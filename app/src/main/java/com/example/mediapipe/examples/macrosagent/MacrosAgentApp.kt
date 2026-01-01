768529b9ac5edec4bc88d27786e62fe78f08f59d
rishisim <rishisim@users.noreply.github.com>
2026-01-01 11:43:11 -0700

    Initialize clean slate for Personal Macros App

--- app/src/main/java/com/example/mediapipe/examples/macrosagent/MacrosAgentApp.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mediapipe.examples.macrosagent
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MacrosAgentApp : Application()
